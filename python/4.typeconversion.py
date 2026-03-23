765b84e0ab71ca85ee6f87c727fd75e3861b48fe
impradip <pradipyadav2438317@gmail.com>
2026-03-23 11:53:23 +0530

    add code file 4

---
+++ python/4.typeconversion.py
@@ -0,0 +1,10 @@
+# Type conversion >>>>>>>>>> 
+# 1.implicit done by python it self 
+     # float+int >>> float 
+a=2;
+b=3.3;
+ans1=a+b;  #type connversion
+print(ans1,type(ans1));
+# 2. explicit done by developer
+sum=int(a+b);  #type castiong
+print(sum,type(sum)); 
