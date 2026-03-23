4d4c44562b626a1c6ccd60a0e454791142ab7d97
impradip <pradipyadav2438317@gmail.com>
2026-03-23 12:19:58 +0530

    add code file 5

--- python/5.inputFormUser.py
+++ python/5.inputFormUser.py
@@ -16,3 +16,10 @@ print(sum,type(sum));
 # input value of a: 3
 # input value 0f b: 2
 # 5 <class 'int'>
+
+#calculate agerage of tow numbers e and f
+e=float(input("enter value of e: "));
+f=float(input("enter value of f: "));
+
+avg=(e+f)/2;
+print("average of the two numbers is: ",avg);
