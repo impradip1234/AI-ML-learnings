cda217ba9bb41e5e8249372f9f2bfcd296315abf
impradip <pradipyadav2438317@gmail.com>
2026-03-23 11:14:18 +0530

    add code file 1 and 2

---
+++ python/1.Basics.py
@@ -0,0 +1,32 @@
+#printing .......
+print('hello mittar')
+
+#next line 
+print('hello \nmittar');
+#output:
+'''
+hello
+mittar
+'''
+print('hello \n mittar');
+#output:
+'''
+hello
+ mittar
+'''
+#variables and data type string and int .....
+name = "Pradip Yadav";
+age = 20;
+print("name: ",name,"\nage: ",age)
+
+#data type of any variable......
+print(type(name))  # <class..str>
+
+#style guide...
+
+#snake_Case
+totPrice=50;
+#camelCase
+totPrice=20;
+#pascalCase
+TotPrice=200;
---
+++ python/2.operators.py
@@ -0,0 +1,45 @@
+#arithmetic
+a=4;
+b=3;
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+
+#relational /comparison
+print(a>b);
+print(b>a);
+print(a==b);
+print(a!=b);
+
+#assignment
+a=20
+a+=5;
+a*=2;
+print(a);
+
+#logical 
+print(not(a>b));
+print((3>2)or(4>9))
+print((3>2)and(4>9))
+
+#bitwise
+c=5
+d=3
+print(c & d)
+print(c | d)
+print(c ^ d)
+print(~c)
+print(c >> 1)
+print(c << 1)
+
+#membership operators .... in and not in....
+
+name="Pradip";
+marks=[1,2,3,4,5];
+print(4 in marks)
+print("p" in name)
+print("P" in name);
+print("a" in name);
+print("A" in name);
