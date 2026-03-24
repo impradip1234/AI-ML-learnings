bb903de75a6901f3980af49564feefb553f8adf1
impradip <pradipyadav2438317@gmail.com>
2026-03-24 23:15:33 +0530

    add code file 8

---
+++ python/8.conditionals.py
@@ -0,0 +1,13 @@
+while(1):
+    color=input("enter color:");
+    if(color=="bas rahe da"):
+        print("band ho gail")
+        break;
+    elif(color=="red"):
+        print("sotp!")
+    elif(color=="green"):
+        print("start")
+    elif(color=="yellow"):
+        print("ready")
+    else:
+        print("mane kuchh hu.....")
