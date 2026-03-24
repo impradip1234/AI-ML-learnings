a6d7ea2a8071c2dd5d4ff0796a422004fa69b375
impradip <pradipyadav2438317@gmail.com>
2026-03-24 23:02:37 +0530

    add code file 7

---
+++ python/7.conditionals.py
@@ -0,0 +1,8 @@
+age=16
+
+if age>18:
+    print("You are eligible for voting.")
+elif age==18:
+    print("you can enrole for voting card.")
+else:
+    print("farziwade mat karna you are not eligible for voting >>>> ghare ja ke sutti babu.")
