1e3c8d08da9cc97bca71a0bb86012967fd89ca9e
impradip <pradipyadav2438317@gmail.com>
2026-03-25 01:31:54 +0530

    add code file 13

---
+++ python/13.whileLoop.py
@@ -0,0 +1,12 @@
+while(1):
+    roll=int(input("enter the roll number: "))
+    name=input("enter name: ")
+    if(roll<1):
+        break;
+    else:
+        print(roll," ",name)
+
+i=5
+while(i>0):
+    print("ghare ha ke sutti babu")
+    i=i-1
