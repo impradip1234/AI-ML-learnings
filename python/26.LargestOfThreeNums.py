47f6c94731817d2db50ec9c0eb5f18522ffaa310
impradip <pradipyadav2438317@gmail.com>
2026-03-25 15:24:43 +0530

    add code file 26

---
+++ python/26.LargestOfThreeNums.py
@@ -0,0 +1,10 @@
+n1=float(input("enter the first number: "))
+n2=float(input("enter the second number: "))
+n3=float(input("enter the third number: "))
+
+if(n1>n2 and n1>n3):
+    print(n1," is largest")
+elif(n2>n1 and n2>n3):
+    print(n2," is largest")
+else:
+    print(n3," is largest")
