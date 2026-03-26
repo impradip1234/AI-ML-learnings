e986d0af0fee2ad41d30b42c42ea2c92b8722f38
impradip <pradipyadav2438317@gmail.com>
2026-03-26 23:14:42 +0530

    add code file 33

---
+++ python/33TuplesMethods.py
@@ -0,0 +1,7 @@
+#method 01 : index method >> return the index of the first occurrence
+tuppu1=(2,3,4,2,2,1,4,2,1)
+print(tuppu1.index(3))
+
+#Method 02 :count method >> return the total occurrence
+tuppu2=(2,3,4,5,1,2,3,4,2,2,6,7,2,4,6,2)
+print(tuppu2.count(2)) 
