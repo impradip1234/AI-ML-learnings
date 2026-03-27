75d1eec617a3c3d1d692b27699dd56f94d22754d
impradip <pradipyadav2438317@gmail.com>
2026-03-27 15:00:01 +0530

    add code file 36

---
+++ python/36.Set.py
@@ -0,0 +1,10 @@
+Name_set={1,2,5,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,4,5}
+print(Name_set) # only the unique elements will be stored : eg: 1,2,3,4,5
+print(len(Name_set)) # 5
+print(type(Name_set)) #<class 'set'>
+
+#creating empty set ....
+set1_empty={}   # this will be a type of dictionary 
+set2_empty=set() #this will be a type of set 
+print(type(set1_empty))  #<class 'dict'>
+print(type(set2_empty)) #<class 'set'>
