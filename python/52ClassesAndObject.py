d810c01aae9fb8fd71e4068b20e6c456891519eb
impradip <pradipyadav2438317@gmail.com>
2026-04-01 23:53:07 +0530

    add code file 52

---
+++ python/52ClassesAndObject.py
@@ -0,0 +1,8 @@
+class Gang:
+    gang_title="garuda"
+    gang_member_name=input("enter the name of the member: ")
+    position_in_gang=input("enter the name of the position: ")
+don1=Gang()
+print(don1.gang_title)
+print(don1.gang_member_name)
+print(don1.position_in_gang)
