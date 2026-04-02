9bb2aa4c351077c8a2ead18333eb19ac9c1ccead
impradip <pradipyadav2438317@gmail.com>
2026-04-02 19:08:29 +0530

    add code file 53

---
+++ python/53.Methods.py
@@ -0,0 +1,24 @@
+#01 : instance method uses self........
+class Students:
+    def __init__(self,name):
+        self.name=name
+    def show (self):
+        return f"Name:{self.name}"
+s=Students("PK")
+print(s.show())
+
+#=static method.....
+class Students:
+    school="ABC school"
+
+    @classmethod
+    def get_school(cls):
+        return cls.school
+print(Students.get_school())
+
+#03: static method..
+class Students:
+    # @staticmethodS
+    def add(a,b):
+        return a+b
+print(Students.add(3,4))
