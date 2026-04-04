e99de1b7d6d8a51d3f18f6a56475d9c54865caf3
impradip <pradipyadav2438317@gmail.com>
2026-04-04 10:33:23 +0530

    add code file 61

---
+++ python/61.Abstraction.py
@@ -0,0 +1,17 @@
+# abstraction is hiding internal details and showing ony essential features
+
+from abc import ABC,abstractmethod
+class Animal(ABC):
+    @abstractmethod
+    def make_sound(self):
+        pass
+class Lion(Animal):
+    def make_sound(self):
+        print("Roar!")
+class Cow(Animal):
+    def make_sound(self):
+        print("Moo!")
+lion=Lion()
+lion.make_sound()
+cow=Cow()
+cow.make_sound()
