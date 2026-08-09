27b5ca80b3241017c3cde3abd40f82c028a0417c
impradip <pradipyadav2438317@gmail.com>
2026-08-09 11:42:03 +0530

    add code file 64, better solution

--- python/64Student.py
+++ python/64Student.py
@@ -1,17 +1,38 @@
-# Create a Student class with attributes _name, _roll_no, and _marks.
-# Provide and methods with validation (e.g., marks cannot be
-# negative, roll number has to be between 1 & 100 & name cannot be empty).
+# Q3. (Encapsulation) 
+# Create a class Student with private attributes_name,_roll_no, and _marks.
+# Provide getter and setter methods with validation( e.g., marks canot be negative, rollnumber has to be between 1 and 100 and name cannot be empty).
 
 class Student:
-    def student(self, name, roll_no, marks):
-        self.name=name
-        self.roll_no=roll_no
-        self.marks=marks
-    def printing(self):
-        if 1 <= self.roll_no <= 100 and self.name != "" and self.marks >= 0:
-            print(self.name,self.roll_no,self.marks)
+    def __init__(self,name, roll_no , marks):
+        self._name=name
+        self._roll_no=roll_no
+        self._marks=marks
+
+    def getter_name(self):
+        return self._name
+    def getter_roll_no(self):
+        return self._roll_no
+    def getter_marks(self):
+        return self._marks
+
+    def setter_name(self,name):
+        if(name==""):
+            print("Invalide name!")
         else:
-            print("roll is not correct or name is empty")
-s1=Student()
-s1.student("Pradip",17,99)
-s1.printing()
+            self._name=name
+    def setter_roll_no(self,roll_no):
+        if(roll_no<1 or roll_no>100):
+            print("Invalid Roll number!")
+        else:
+            self._roll_no=roll_no
+    def setter_marks(self,marks):
+        if(marks<0):
+            print("Invalid marks!")
+        else:
+            self._marks=marks
+    
+        
+s1=Student("pradip",44,99)
+s2=Student("a",1,90)
+s2.setter_roll_no(99)
+print(s2.getter_roll_no())
